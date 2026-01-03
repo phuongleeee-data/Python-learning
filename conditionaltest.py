a6cbb14a533836bccf8615f5781fa6ef451b3401
Lê Ngọc Minh Phương <minhphuong6417@gmail.com>
2026-01-03 14:33:45 +0700

    Add files via upload

---
+++ conditionaltest.py
@@ -0,0 +1,12 @@
+print('Test case 1')
+car = 'bmw'
+
+print('Is car == "bmw"? I predict True.')
+print(car == 'bmw')
+
+print('\nTest case 2')
+car = 'audi'
+print('\nIs car == "bmw"? I predict False.')
+print(car == 'bmw')
+
+print('\nTest case 3')
