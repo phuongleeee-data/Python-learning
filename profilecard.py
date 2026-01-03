8b62ce6e1a95caf17cc9e3ca6909aaffdeec949d
Lê Ngọc Minh Phương <minhphuong6417@gmail.com>
2026-01-03 14:31:52 +0700

    Add files via upload

---
+++ profilecard.py
@@ -0,0 +1,13 @@
+print("===HỒ SƠ ỨNG VIÊN THẠC SĨ BÁCH KHOA===")
+first_name = "Phương"
+middle_name = "Ngọc Minh"
+last_name = "Lê"
+home_town ="Đồng Nai"
+birth_year = 2005
+major="Computer Science"
+full_name = last_name + " " + middle_name + " " + first_name
+
+print(f"Họ và tên: {full_name.title()}")
+print(f"Tuổi: {2025 - birth_year}")
+print(f"Quê quán: {home_town.upper()}")
+print(f"Chuyên ngành: {major}")
