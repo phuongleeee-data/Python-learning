d2e96b91451d5e9f79b104eaa0d4da0031952ef2
Lê Ngọc Minh Phương <minhphuong6417@gmail.com>
2026-01-03 14:33:26 +0700

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,19 @@
+suspicious_ip = ['192.168.1.1', '10.0.0.5', '172.16.0.1', '192.168.1.10', '172.16.0.4']
+print(suspicious_ip)
+
+print(sorted(suspicious_ip))
+print(suspicious_ip)
+
+print(sorted(suspicious_ip, reverse=True))
+
+suspicious_ip.reverse()
+print(suspicious_ip)
+
+suspicious_ip.reverse()
+print(suspicious_ip)
+
+suspicious_ip.sort()
+print(suspicious_ip)
+
+suspicious_ip.sort(reverse=True)
+print(suspicious_ip)
