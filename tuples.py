5132b8243a851a11615ac3f202dfc599d05aff7b
Lê Ngọc Minh Phương <minhphuong6417@gmail.com>
2026-01-03 14:32:44 +0700

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,8 @@
+foods = ('hot pot', 'beef', 'fish', 'pork', 'topping')
+for food in foods:
+    print(f"The original menu has {food}")
+    
+
+foods = ('BBQ sauce', 'beef', 'fish', 'pork', 'chicken', 'chips')
+for food in foods:
+    print(f"The updated tuple of foods are {food}")
